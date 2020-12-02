e0814a9fd44fd888d933f3389448b33619247b05
Thales Magalhaes <thls.mglhs@gmail.com>
2020-12-02 15:20:24 -0300

    Add solutions for day 2

---
+++ 2020/ThalesII/day-2/part-1.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(define (string-count str char)
+  (count (lambda (c) (char=? c char)) (string->list str)))
+
+(define (valid-entry? entry)
+  (match-define (list lo hi char pass) entry)
+  (<= lo (string-count pass char) hi))
+
+(define (string->entry str)
+  (match-define (list _ lo-str hi-str char-str pass)
+    (regexp-match #px"^(\\d+)-(\\d+) (.): (.+)$" str))
+  (list (string->number lo-str)
+        (string->number hi-str)
+        (string-ref char-str 0)
+        pass))
+
+(define (port->entries)
+  (map string->entry (port->lines)))
+
+(let ([entries (port->entries)])
+  (count valid-entry? entries))
