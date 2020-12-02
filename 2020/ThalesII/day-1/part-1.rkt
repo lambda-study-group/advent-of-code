a3b4d7fb9fe0388a7abeeeccb0d9883bc907e305
Thales Magalhaes <thls.mglhs@gmail.com>
2020-12-02 01:38:38 -0300

    Simplify solutions from day 1

--- 2020/ThalesII/day-1/part-1.rkt
+++ 2020/ThalesII/day-1/part-1.rkt
@@ -1,19 +1,13 @@
 #lang racket
 
-(define (tails lst)
-  (cond
-    [(empty? lst) '(())]
-    [else (cons lst (tails (rest lst)))]))
-
 (define (solve lst)
-  (for/or ([xs (tails lst)]
-           #:unless (empty? xs)
-           [y (rest xs)]
-           #:when (= (+ (first xs) y) 2020))
-    (* (first xs) y)))
+  (for*/or ([x lst]
+            [y lst]
+             #:when (= (+ x y) 2020))
+      (* x y)))
 
 (define (port->numbers [port (current-input-port)])
   (map string->number (port->lines port)))
 
 (let [(input (port->numbers))]
-    (displayln (solve input)))
+    (solve input))
--- 2020/ThalesII/day-1/part-2.rkt
+++ 2020/ThalesII/day-1/part-2.rkt
@@ -1,21 +1,14 @@
 #lang racket
 
-(define (tails lst)
-  (cond
-    [(empty? lst) '(())]
-    [else (cons lst (tails (rest lst)))]))
-
 (define (solve lst)
-  (for/or ([xs (tails lst)]
-           #:unless (empty? xs)
-           [ys (tails (rest xs))]
-           #:unless (empty? ys)
-           [z (rest ys)]
-           #:when (= (+ (first xs) (first ys) z) 2020))
-    (* (first xs) (first ys) z)))
+  (for*/or ([x lst]
+            [y lst]
+            [z lst]
+            #:when (= (+ x y z) 2020))
+    (* x y z)))
 
 (define (port->numbers [port (current-input-port)])
   (map string->number (port->lines port)))
 
 (let ([input (port->numbers)])
-  (displayln (solve input)))
+  (solve input))
