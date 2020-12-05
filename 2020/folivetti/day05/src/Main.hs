467360657e2d96f93f390fead7d8d6ac86ae824c
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2020-12-05 08:00:16 -0300

    small refactor

--- 2020/folivetti/day05/src/Main.hs
+++ 2020/folivetti/day05/src/Main.hs
@@ -3,29 +3,14 @@ module Main where
 import Data.Char      (digitToInt)
 import Numeric        (readInt)
 import Data.List      (sort)
-import Data.Bifunctor
 
-parse2bin :: (Char -> Char) -> String -> Int
-parse2bin parser = fst . head . readInt 2 (`elem` "01") digitToInt . map parser
-
-parserFB :: Char -> Char
-parserFB 'F' = '0'
-parserFB 'B' = '1'
-parserFB  _  = error "Invalid character in parserFB"
-
-parserRL :: Char -> Char
-parserRL 'R' = '1'
-parserRL 'L' = '0'
-parserRL  _  = error "Invalid character in parserRL"
-
-parseSeat :: String -> (Int, Int)
-parseSeat = bimap fb rl . splitAt 7
+getId :: String -> Int
+getId = fst . head . readInt 2 (`elem` "01") digitToInt . map parser
   where
-    fb = parse2bin parserFB
-    rl = parse2bin parserRL
-
-getId :: (Int, Int) -> Int
-getId (x,y) = x * 8 + y
+    parser 'F' = '0'
+    parser 'B' = '1'
+    parser 'R' = '1'
+    parser 'L' = '0'
 
 findId :: [Int] -> Maybe Int
 findId = go . sort
@@ -38,6 +23,6 @@ findId = go . sort
 main :: IO ()
 main = do
   contents <- lines <$> readFile "day05.txt"
-  let seats = map (getId.parseSeat) contents
+  let seats = map getId contents
   print $ maximum seats
   print $ findId seats
