31130032410168aa287dadadcf4b3d2384609d05
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2020-12-15 11:35:03 -0300

    mutable

--- 2020/folivetti/day15/src/Main.hs
+++ 2020/folivetti/day15/src/Main.hs
@@ -1,6 +1,10 @@
+{-# language FlexibleContexts #-}
 module Main where
 
 import Data.IntMap.Strict
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
 
 genList [x]    n s = x : next    0  (n+1) (insert x n s)
 genList (x:xs) n s = x : genList xs (n+1) (insert x n s)
@@ -9,10 +13,23 @@ next x n s =
   case s !? x of
     Nothing -> x : next 0     (n+1) (insert x n s)
     Just y  -> x : next (n-y) (n+1) (insert x n s)
-               
+
+run :: Int -> [Int] -> Int
+run n input = runST $ do
+  arr <- newArray (0, n) (-1) :: ST s (STUArray s Int Int)
+  let init (i, v) = writeArray arr v i
+      f next turn = do val <- readArray arr next
+                       writeArray arr next turn
+                       if val == -1 
+                          then return 0
+                          else return (turn-val)
+  mapM_ init $ zip [1..] input 
+  foldM f 0 [length input + 1 .. n - 1]
+
 main :: IO ()
 main = do
-  let list = genList [15,12,0,14,3,1] 1 empty
-  print $ take 100 list
-  print $ list !! (2020 - 1)
-  print $ list !! (30000000 - 1)
+  --let list = genList [15,12,0,14,3,1] 1 empty
+  --print $ list !! (2020 - 1)
+  --print $ list !! (30000000 - 1)
+  print $ run 2020 [15,12,0,14,3,1]
+  print $ run 30000000 [15,12,0,14,3,1]
