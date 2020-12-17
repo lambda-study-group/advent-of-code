023ed4ef65f7272526739602151d8e733a935d14
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2020-12-17 10:24:17 -0300

    day17

--- 2020/folivetti/day17/src/Main.hs
+++ 2020/folivetti/day17/src/Main.hs
@@ -2,79 +2,57 @@ module Main where
 
 import Data.Set (Set(..), member)
 import qualified Data.Set as S
+import qualified Data.Map.Strict as M
 
-data Cell    = Inactive | Active deriving (Eq, Show)
-type Coord   = (Int, Int, Int)
-type Coord4D = (Int, Int, Int, Int)
+type Coord = (Int, Int, Int, Int)
 
 -- * Parsing
-
-parse :: (Int -> String -> [a]) -> String -> [a]
-parse pLine input = concat
-                  $ zipWith pLine [0..]
-                  $ lines input
-
-parseLine :: Int  -> String -> [Coord]
-parseLine x css = foldr getActive []
-                $ zip [0..]
-                $ map parseChar css
+parse :: String -> Set Coord
+parse = go 0 0 S.empty
   where
-    getActive (_, Inactive) xs = xs
-    getActive (y, Active)   xs = (x,y,0):xs
-
-
-parseLine4D :: Int  -> String -> [Coord4D]
-parseLine4D x css = foldr getActive []
-                  $ zip [0..]
-                  $ map parseChar css
-  where
-    getActive (_, Inactive) xs = xs
-    getActive (y, Active)   xs = (x,y,0,0):xs
-
-parseChar :: Char -> Cell
-parseChar '.' = Inactive
-parseChar '#' = Active
-parseChar  _  = error "invalid char"
+    go x y s ('#' :cs) = go (x+1) y     (S.insert (x,y,0,0) s) cs
+    go x y s ('.' :cs) = go (x+1) y     s                      cs
+    go _ y s ('\n':cs) = go 0     (y+1) s                      cs
+    go _ _ s []        = s
 
 -- * Update Automata
-update :: (Ord a) => (a -> [a]) -> Set a -> Set a
-update neighFun s = S.fromList $ coordsActives ++ coordsInactives
+update :: (Set Coord -> M.Map Coord Int) -> Set Coord -> Set Coord
+update neigh s = S.union s1 s2
   where
-    coords          = S.toList s
-    coordsActives   = filter ((`elem`[2,3]).getActiveCoords) coords
-    coordsInactives = concatMap getInactiveCoords coords
+    ns          = neigh s
+    isActive xs = M.keysSet . M.filter (`elem` xs)
+    -- active neighborhood
+    s1 = isActive [2,3] $ M.restrictKeys ns s
+    -- inactive neighborhood
+    s2 = isActive [3]   $ M.withoutKeys ns s
 
-    getActiveCoords   coord = let activeNeigh = filter (`member` s) $ neighFun coord
-                              in  length activeNeigh
-
-    getInactiveCoords coord = let inactiveNeigh = filter (`notMember` s) $ neighFun coord
-                              in  filter ((==3).getActiveCoords) inactiveNeigh
-
-notMember :: Ord a => a -> Set a -> Bool
-notMember x s = not (member x s)
-
-neighbors :: Coord -> [Coord]
-neighbors (x, y, z) = sum3D (x,y,z) <$> filter (/=(0,0,0)) coords
+-- returns a map counting the number of active neighbors for each
+-- active coordinate and their neighbors
+neighbors :: [Coord] -> Set Coord -> M.Map Coord Int
+neighbors ds s = M.unionsWith (+) 
+               $ map (forKeys m . sumCoord) ds
   where
-    bounds = [-1..1]
-    coords = (,,) <$> bounds <*> bounds <*> bounds
-    sum3D (x,y,z) (a,b,c) = (x+a, y+b, z+c)
+    forKeys = flip M.mapKeys
+    m       = M.fromSet (const 1) s
 
-neighbors4D :: Coord4D -> [Coord4D]
-neighbors4D (x, y, z, w) = sum4D (x,y,z,w) <$> filter (/=(0,0,0,0)) coords
-  where
-    bounds = [-1..1]
-    coords = (,,,) <$> bounds <*> bounds <*> bounds <*> bounds
-    sum4D (x,y,z,w) (a,b,c,d) = (x+a, y+b, z+c, w+d)
+sumCoord :: Coord -> Coord -> Coord
+sumCoord (x,y,z,w) (a,b,c,d) = (x+a,y+b,z+c,w+d)
 
-example = ".#.\n..#\n###"
+deltas :: [Coord]
+deltas = filter (/=(0,0,0,0))
+       $ (,,,) <$> [-1..1] <*> [-1..1] <*> [-1..1] <*> [0]
+
+deltas4D :: [Coord]
+deltas4D = filter (/=(0,0,0,0))
+         $ (,,,) <$> [-1..1] <*> [-1..1] <*> [-1..1] <*> [-1..1]
 
 main :: IO ()
 main = do
   dat <- readFile "day17.txt"
-  let s0     = S.fromList $ parse parseLine dat
-      s04D   = S.fromList $ parse parseLine4D dat
-      sets   = iterate (update neighbors) s0
-      sets4D = iterate (update neighbors4D) s04D
+  let s0     = parse dat
+      n3D    = neighbors deltas
+      n4D    = neighbors deltas4D
+      sets   = iterate (update n3D) s0
+      sets4D = iterate (update n4D) s0
   print $ S.size (sets !! 6)
   print $ S.size (sets4D !! 6)
