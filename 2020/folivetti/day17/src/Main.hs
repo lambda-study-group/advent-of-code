a40e258c4e83f72a58f71ccf203f5d4e69af309d
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2020-12-18 13:43:48 -0300

    day18

--- 2020/folivetti/day17/src/Main.hs
+++ 2020/folivetti/day17/src/Main.hs
@@ -16,10 +16,10 @@ parse = go 0 0 S.empty
     go _ _ s []        = s
 
 -- * Update Automata
-update :: (Set Coord -> M.Map Coord Int) -> Set Coord -> Set Coord
-update neigh s = S.union s1 s2
+update :: [Coord] -> Set Coord -> Set Coord
+update ds s = S.union s1 s2
   where
-    ns          = neigh s
+    ns          = neighbors ds s
     isActive xs = M.keysSet . M.filter (`elem` xs)
     -- active neighborhood
     s1 = isActive [2,3] $ M.restrictKeys ns s
@@ -30,10 +30,10 @@ update neigh s = S.union s1 s2
 -- active coordinate and their neighbors
 neighbors :: [Coord] -> Set Coord -> M.Map Coord Int
 neighbors ds s = M.unionsWith (+) 
-               $ map (forKeys m . sumCoord) ds
+               $ map (forKeys m . sumCoord) ds -- each map has a coord as key and the value is the number of active neighbors
   where
-    forKeys = flip M.mapKeys
-    m       = M.fromSet (const 1) s
+    forKeys = flip M.mapKeys        -- for each key (coord) from a map, apply a function
+    m       = M.fromSet (const 1) s -- every active cell has value 1
 
 sumCoord :: Coord -> Coord -> Coord
 sumCoord (x,y,z,w) (a,b,c,d) = (x+a,y+b,z+c,w+d)
@@ -50,9 +50,7 @@ main :: IO ()
 main = do
   dat <- readFile "day17.txt"
   let s0     = parse dat
-      n3D    = neighbors deltas
-      n4D    = neighbors deltas4D
-      sets   = iterate (update n3D) s0
-      sets4D = iterate (update n4D) s0
+      sets   = iterate (update deltas) s0
+      sets4D = iterate (update deltas4D) s0
   print $ S.size (sets !! 6)
   print $ S.size (sets4D !! 6)
